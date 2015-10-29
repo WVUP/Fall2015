a6a093503d23bf1c4b5dec806d0f87bc34aa2e9f
Matt Gable <matthewgable@gmail.com>
2015-10-28 22:46:31 -0400

    A few more updates
    -Added a small key-querying function (added to an onclick)
    -Finished very early first draft of left button column
    -Added a clone column of buttons as placeholders

--- Custom Project/Gable/DummyAmazonData.js
+++ Custom Project/Gable/DummyAmazonData.js
@@ -1,6 +1,7 @@
-//Dummy data objects
+//Dummy data object records
 
-var record1 = {
+var record1 = 
+{
 	ProductID: 'FinalFantasy7',
 	SKU: '12774',
 	Price: '50',
@@ -10,15 +11,7 @@ var record1 = {
 	Tags: 'video game, vintage, FF'
 }
 
-
-// function printAmazonRecords()
-// {
-// 	document.write('</br> Vendor: AMAZON');
-// 	document.write('</br>Product ID: ' + record1.ProductID);
-// 	document.write('</br>Name of the Product: ' + record1.Name);
-// 	document.write('</br>Price of the Product: ' + record1.Price);
-// 	document.write('</br>Number sold: ' + record1.NumberSold);
-// }
+//Functions
 
 function giveAmazonRecords()
 {
@@ -52,4 +45,9 @@ function giveBuyRecommendation()
 	{
 		return 'Buy? No';
 	}
+}
+
+function giveRawRecordKeys ()
+{
+	return Object.keys(record1);
 }
