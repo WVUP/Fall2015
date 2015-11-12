63f9ba05cbb788fbfe84ff287d1b7b33825e9c38
Matt Gable <matthewgable@gmail.com>
2015-11-12 00:06:27 -0500

    Added a couple comparative functions
    -Added functionality for preference calculations based on the Preferred
    Products cart and also Price difference for the item of interest

--- Custom Project/Gable/ComparativeFunctions.js
+++ Custom Project/Gable/ComparativeFunctions.js
@@ -2,7 +2,32 @@
 
 
 function giveAverage(){
-
 	return "The mean price between these two vendors is: $" + 
 	(amazonRecords[0].Price + ebayRecords[0].Price) / 2;
+}
+
+function givePriceDifference(){
+	if (amazonRecords[0].Price > ebayRecords[0].Price){
+		return "Price difference: For this item, Amazon is more expensive by $" + 
+		(amazonRecords[0].Price - ebayRecords[0].Price);
+	} else if (ebayRecords[0].Price > amazonRecords[0].Price){
+		return "Price difference: For this item, Ebay is more expensive by $" + 
+		(ebayRecords[0].Price - amazonRecords[0].Price);
+	} else {
+		return "Incomplete data. Please try another query.";
+	}
+}
+
+function givePreferenceRating(){
+	if (amazonpreferredProducts.length > ebaypreferredProducts.length){
+		return "Your Preferred Products favor Amazon by " + 
+		(amazonpreferredProducts.length - ebaypreferredProducts.length) + 
+		" units";
+	} else if (ebaypreferredProducts.length > amazonpreferredProducts.length){
+		return "Your Preferred Products favor Ebay by " + 
+		(ebaypreferredProducts.length - amazonpreferredProducts.length) + 
+		" units";
+	} else {
+		return "Your Preferred Products cart has no preference yet.";
+	}	
 }
