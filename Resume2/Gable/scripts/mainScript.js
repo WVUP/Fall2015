3b52e507bfde2a4ee92c3fa3e52e44ca244201b9
Matt Gable <matthewgable@gmail.com>
2015-11-26 16:41:05 -0500

    -JSON...
    -Finally got the raw form input----->object literal process  built in.
    Added a (probably too large) function that iterates over form input,
    stringifies it into the proper format, then parses into an object
    literal for appending to a guestbook array.
    -TODO-A lot! (Up next: some formatting and building up content before
    moving toward cleaning up functions and putting all the pieces together.

--- Resume2/Gable/scripts/mainScript.js
+++ Resume2/Gable/scripts/mainScript.js
@@ -1,13 +1,6 @@
-var guests = [
-
-
+guests = [{"name" : "value"},
 
 ];
-
-
-
-
-
 
 //Main functions
 
@@ -42,7 +35,36 @@ function giveFormData () {
 
 };
 
-//Stringify form data
+//TODO: Stringify form data
+
+//Get the form data, iterate over it and stringify it into JSON form, 
+//then return an object literal.
+function giveJSON () {
+
+	var input = document.getElementById('form1');
+	var output = "";
+	var i;
+	output += "{";
+	for (i = 0; i < input.length; i++){
+		if (i == input.length - 1){
+			output += '"' + input.elements[i].name + '"' + ' : ' + '"' + input.elements[i].value + '"';
+			break;
+		} else
+		output += '"' + input.elements[i].name + '"' + ' : ' + '"' + input.elements[i].value + '"' + ' , ';
+	};
+	output += "}";
+
+	//JSON.stringify(output);
+	console.log(JSON.parse(output));
+
+	var JSONobj = JSON.parse(output);
+
+	//guests.push("it worked");
+
+	return JSONobj;
+
+};
+
 
 function getData2 () {
 
