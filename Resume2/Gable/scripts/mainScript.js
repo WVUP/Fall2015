fbc490ef7704391291415e85dd9b2753dc3d186f
Matt Gable <matthewgable@gmail.com>
2015-11-25 19:51:56 -0500

    A few steps toward handling data
    -Added an array-based function to capture strings via index
    -Added a few small testing functions for form data
    -Added a very basic form template

--- Resume2/Gable/scripts/mainScript.js
+++ Resume2/Gable/scripts/mainScript.js
@@ -1,11 +1,56 @@
+var guests = [
+
+
+
+];
+
+
+
+
+
+
 //Main functions
 
 function printStuff(){
 
 	document.write("hello2");
+
 }
 
 //Topnav functions
+
 function goToGuestbook () {
+
 	location.href = 'guestbook.html';
-}
+
+};
+
+//Form functions
+
+//Capture and test form data
+function giveFormData () {
+
+	var input = document.getElementById('form1');
+	var output = "";
+	var i;
+	for (i = 0; i < input.length; i++){
+		output += input.elements[i].value + "</br>";
+	}
+	//writing wach element to a div on a new line
+	console.log(output);
+	document.getElementById('testformtext').innerHTML = output;
+
+};
+
+//Stringify form data
+
+function getData2 () {
+
+	var input = document.getElementById('form1');
+	document.getElementById('testformtext').innerHTML = input;
+};
+
+function writeBasic () {
+	document.getElementById("testformtext").innerHTML = "testttt";
+
+};
