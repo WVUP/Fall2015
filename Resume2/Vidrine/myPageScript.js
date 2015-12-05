659a6ca4e39c17fda246f6ee6e1aa3c25f1c8e5c
Isaac Vidrine <ividrine@wvup.edu>
2015-12-05 16:34:02 -0500

    update
    page update

--- Resume2/Vidrine/myPageScript.js
+++ Resume2/Vidrine/myPageScript.js
@@ -1,20 +1,59 @@
-function getAboutMe () {
-	var container = document.getElementById('displayArea');
-	$.ajax({
-		url: 'https://raw.githubusercontent.com/WVUP/Fall2015/master/Resume2/Vidrine/about.html',
-		type: 'GET',
-		success: function (resp) {
-			container.innerHTML = resp
-		}
-	})
 
-}
-
-
-$(document).on( 'ready', function () {
+$(document).on('ready', function () {
+	
 	$('#about').on( 'click', function () {
-		getAboutMe()
-		alert("Fudge Yeah, I'm a mac genius yo.....but na really im a nub tho")
+	
+		var dialog = bootbox.alert('<div style="font-size:200%;"><i class="fa fa-refresh fa-spin"></i> Loading data</div>');
+
+		setTimeout(function () {
+			var container = document.getElementById('aboutArea')
+			$.ajax({
+				url: 'https://raw.githubusercontent.com/WVUP/Fall2015/master/Resume2/Vidrine/about.html',
+				type: 'GET',
+				success: function (resp) {
+					container.innerHTML = resp
+				},
+				error: function (err) {
+					console.log("Error")
+					console.log(err)
+				},
+				complete: function () {
+					console.log("complete")
+					setTimeout(function () {
+						dialog.modal('hide');
+					}, 100)
+				}
+			})
+		}, 1000)
 	});
+
+	$('#resume').on('click', function () {
+
+		var dialog = bootbox.alert('<div style="font-size:200%;"><i class="fa fa-refresh fa-spin"></i> Loading data</div>');
+		
+		setTimeout(function () {
+
+			var container = document.getElementById('aboutArea');
+
+			$.ajax({
+				url: 'https://raw.githubusercontent.com/WVUP/Fall2015/master/Resume2/Vidrine/isaacResume.html',
+				type: 'GET',
+				success: function (response) {
+					container.innerHTML = response
+				},
+				error: function (err) {
+					console.log('Error')
+					console.log(err)
+				},
+				complete: function () {
+					console.log('complete')
+					setTimeout(function () {
+						dialog.modal('hide');
+					}, 100)
+				}
+			})
+
+		}, 1000)
+	})
 	
 });
