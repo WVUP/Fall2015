cd9d5caf65903d274aa6d416c6296a68824cdbe0
Casey Murphy <st01k@lvaas.net>
2015-12-10 01:26:11 -0500

    resume2
    final project - in progress

---
+++ Resume2/Murphy/config/scripts.js
@@ -0,0 +1,12 @@
+// 9737-9741
+// 9764-9772
+// 9774-9784
+// 9788-9811
+// 9866-9871
+// 9907-9916
+// http://www.w3schools.com/charsets/ref_utf_misc_symbols.asp
+
+bootbox.dialog({
+  title: "That html",
+  message: '<img src="images/bootstrap_logo.png" width="100px"/><br/> You can also use <b>html</b>'
+});
