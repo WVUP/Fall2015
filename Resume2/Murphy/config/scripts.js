13cfb75449802a5344f0f0d4a4ba76c7147adc78
Casey Murphy <st01k@lvaas.net>
2015-12-10 04:56:46 -0500

    resume2
    modals working for nav
    tooltips display correctly - need reposition
    added blog.html

--- Resume2/Murphy/config/scripts.js
+++ Resume2/Murphy/config/scripts.js
@@ -1,17 +1,9 @@
-// 9737-9741
-// 9764-9772
-// 9774-9784
-// 9788-9811
-// 9866-9871
-// 9907-9916
+// 9737-9741, 9764-9772, 9774-9784, 9788-9811
+// 9866-9871, 9907-9916
 // http://www.w3schools.com/charsets/ref_utf_misc_symbols.asp
 
-bootbox.dialog({
-  title: "That html",
-  message: '<img src="images/bootstrap_logo.png" width="100px"/><br/> You can also use <b>html</b>'
+$(document).ready(function(){
+	// enable all tooltips in doc
+  $('[data-toggle="tooltip"]').tooltip(); 
 });
 
-// enable all tooltips in doc
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
-});
