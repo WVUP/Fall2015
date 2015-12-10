2eb47ae261838b737e421b231d50778e9e8fec50
Isaac Vidrine <ividrine@wvup.edu>
2015-12-10 17:40:16 -0500

    update
    Another update almost finished

--- Resume2/Vidrine/myPageScript.js
+++ Resume2/Vidrine/myPageScript.js
@@ -12,7 +12,10 @@ $(document).on('ready', function () {
 				type: 'GET',
 				success: function (resp) {
 					container.innerHTML = resp
-
+					document.getElementById('about').className="active";
+					document.getElementById('resume').className="poop";
+					document.getElementById('demos').className="poop";
+					document.getElementById('contact').className="poop";
 				},
 				error: function (err) {
 					console.log("Error")
@@ -41,6 +44,76 @@ $(document).on('ready', function () {
 				type: 'GET',
 				success: function (response) {
 					container.innerHTML = response
+					document.getElementById('resume').className="active";
+					document.getElementById('about').className="poop";
+					document.getElementById('contact').className="poop";
+					document.getElementById('demos').className="poop";
+				},
+				error: function (err) {
+					console.log('Error')
+					console.log(err)
+				},
+				complete: function () {
+					console.log('complete')
+					setTimeout(function () {
+						dialog.modal('hide');
+					}, 100)
+				}
+			})
+
+		}, 1000)
+	});
+
+	$('#contact').on('click', function () {
+
+		var dialog = bootbox.alert('<div style="font-size:200%;"><i class="fa fa-refresh fa-spin"></i> Loading data</div>');
+		
+		setTimeout(function () {
+
+			var container = document.getElementById('displayArea');
+
+			$.ajax({
+				url: 'https://raw.githubusercontent.com/WVUP/Fall2015/master/Resume2/Vidrine/contact.html',
+				type: 'GET',
+				success: function (response) {
+					container.innerHTML = response
+					document.getElementById('contact').className="active";
+					document.getElementById('about').className="poop";
+					document.getElementById('resume').className="poop";
+					document.getElementById('demos').className="poop";
+				},
+				error: function (err) {
+					console.log('Error')
+					console.log(err)
+				},
+				complete: function () {
+					console.log('complete')
+					setTimeout(function () {
+						dialog.modal('hide');
+					}, 100)
+				}
+			})
+
+		}, 1000)
+	});
+
+	$('#demos').on('click', function () {
+
+		var dialog = bootbox.alert('<div style="font-size:200%;"><i class="fa fa-refresh fa-spin"></i> Loading data</div>');
+		
+		setTimeout(function () {
+
+			var container = document.getElementById('displayArea');
+
+			$.ajax({
+				url: 'https://raw.githubusercontent.com/WVUP/Fall2015/master/Resume2/Vidrine/demos.html',
+				type: 'GET',
+				success: function (response) {
+					container.innerHTML = response
+					document.getElementById('demos').className="active";
+					document.getElementById('about').className="poop";
+					document.getElementById('resume').className="poop";
+					document.getElementById('contact').className="poop";
 				},
 				error: function (err) {
 					console.log('Error')
